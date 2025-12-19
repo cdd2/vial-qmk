6e2b2161ee1eaba4ae018c71538a86582c3ad701
characterlimi <website@silversword.dev>
2025-12-18 21:05:06 -0600

    Added vial support for bounce 75 hotswap keyboard

---
+++ keyboards/kbdfans/bounce/75/hotswap/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xBB, 0x73, 0xB6, 0x56, 0x06, 0x53, 0x6D, 0x35}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
